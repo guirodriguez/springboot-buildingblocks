8022675afed5ce82a0cfa1bcb4c992c0ffa0aeaf
GuiRodriguez <GuiRodriguez@SLSMGLINBP04016.swm.com.ar>
2022-10-25 19:17:35 -0300

    segundo commit prueba git staging arreglo

--- src/main/java/com/stacksimplify/restservices/DemoApplication.java
+++ src/main/java/com/stacksimplify/restservices/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-//segundo commit
+//segundo commit prueba
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
